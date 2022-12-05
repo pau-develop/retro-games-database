e9b12c780b42d33c0572577b950ef803a14527ce
Pau <103753396+pau-develop@users.noreply.github.com>
2022-12-06 00:51:39 +0100

    Created and tested Form components jsx elements

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,5 +1,18 @@
+import FormStyled from "./FormStyled";
+
 const Form = (): JSX.Element => {
-  return <section></section>;
+  return (
+    <FormStyled>
+      <label htmlFor="username">User name</label>
+      <input id="username"></input>
+      <label htmlFor="password">Password</label>
+      <input id="password" type="password"></input>
+      <label htmlFor="repassword">Repeat password</label>
+      <input id="repassword" type="password"></input>
+      <label htmlFor="email">Email</label>
+      <input id="email"></input>
+    </FormStyled>
+  );
 };
 
 export default Form;
--- components/Layout/LayoutStyled.ts
+++ components/Layout/LayoutStyled.ts
@@ -4,6 +4,10 @@ const LayoutStyled = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   .layout {
   }
 `;
