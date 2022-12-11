70b7d1455a1b98f953b34e8fd44836ec33bb7849
Pau <103753396+pau-develop@users.noreply.github.com>
2022-12-11 15:11:21 +0100

    Tested missing functions in useUserAPI hook

--- __tests__/hooks/useUserAPI.test.ts
+++ __tests__/hooks/useUserAPI.test.ts
@@ -37,4 +37,40 @@ describe("Given a useAPI hook", () => {
       expect(result).toBe(true);
     });
   });
+
+  describe("When its function 'userLogin' is called", () => {
+    test("If either the user name or password are wrong, it will return false", async () => {
+      const { userLogin } = useUserAPI();
+      global.fetch = jest.fn().mockResolvedValue({
+        status: 403,
+      });
+      const result = await userLogin("test", "Test1");
+      expect(result).toBe(false);
+    });
+    test("If all info is correct, it should return the response", async () => {
+      const { userLogin } = useUserAPI();
+      global.fetch = jest.fn().mockResolvedValue({
+        status: 200,
+      });
+      const result = await userLogin("test", "Test1");
+      expect(result).toStrictEqual({ status: 200 });
+    });
+  });
+
+  describe("When its function 'userRegister' is called", () => {
+    const userData = {
+      userName: "tester",
+      password: "Test1",
+      rePassword: "Test1",
+      email: "test@test.com",
+    };
+    test("It will return ", async () => {
+      const { userRegister } = useUserAPI();
+      global.fetch = jest.fn().mockResolvedValue({
+        status: 200,
+      });
+      const result = await userRegister(userData);
+      expect(result).toStrictEqual({ status: 200 });
+    });
+  });
 });
