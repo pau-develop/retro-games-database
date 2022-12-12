7d594d066edf5a36a2d458b368430b1f9ae43bde
Pau <103753396+pau-develop@users.noreply.github.com>
2022-12-12 15:43:53 +0100

    Wrapped app around ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,20 @@ import type { AppProps } from "next/app";
 import "../styles/global.css";
 import { Provider } from "react-redux";
 import { store } from "store/store";
+import { ThemeProvider } from "styled-components";
+import theme from "theme/theme";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Provider store={store}>
-        <Header />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Provider>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <Header />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
+      </ThemeProvider>
     </>
   );
 }
---
+++ theme/theme.ts
@@ -0,0 +1,6 @@
+const theme = {
+  primaryColor: "#256D85",
+  fontColor: "#B1D5CB",
+};
+
+export default theme;
