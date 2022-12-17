56aa173e513886979b218b03c423d2cfa62a1e36
Pau <103753396+pau-develop@users.noreply.github.com>
2022-12-17 12:20:24 +0100

    Made some changes to style

--- components/Layout/LayoutStyled.ts
+++ components/Layout/LayoutStyled.ts
@@ -2,14 +2,11 @@ import styled from "styled-components";
 
 const LayoutStyled = styled.div`
   width: 100%;
-  height: 100%;
-  min-height: 100%;
-  background-color: black;
+  height: 94%;
+
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 0 20px;
-  padding-top: 100px;
 
   .layout {
   }
